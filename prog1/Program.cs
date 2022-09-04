b00eabd6ec79352c439eb8c96a58e8414c4d01a6
Anatoly <up.many.money@gmail.com>
2022-09-04 19:23:41 +0300

    Create prog1

---
+++ prog1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные,
+// и наоборот.
+
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(-8, 9);
+}
+var str = string.Join(", ", array);
+Console.WriteLine(str);
+
+foreach (int item in array)
+{
+    int get = item * -1;
+    Console.Write(get + ", ");
+}
