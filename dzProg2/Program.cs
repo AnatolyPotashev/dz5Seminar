8ec3ed4994f8b7f3ba17c33155a18b8b0fea282c
Anatoly <up.many.money@gmail.com>
2022-09-06 15:42:11 +0300

    Beta prog2

---
+++ dzProg2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[10];
+for (int i = 0; i < 10; i++)
+{
+    array[i] = new Random().Next(1, 100);
+}
+var str = string.Join(", ", array);
+Console.WriteLine(str);
+
+int res = 0;
+foreach (int item in array)
+{
+    if (item % 2 == 1) res = item + res;
+}
+
+Console.WriteLine(res);
